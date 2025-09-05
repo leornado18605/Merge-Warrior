7abf66a2b580dc0474fec30a36ed7899dba6cb93
Le Tran Gia Bao <letrangiabao18062k5@gmail.com>
2025-09-05 11:09:00 +0700

    Scene Load

---
+++ Assets/LoadingCircle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LoadingCircle : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 180f; 
+
+    void Update()
+    {
+        transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/SplashController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    [SerializeField] private float splashDuration = 2.5f; 
+    [SerializeField] private string nextSceneName = "Level1"; 
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextSceneAfterDelay());
+    }
+
+    private System.Collections.IEnumerator LoadNextSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(splashDuration);
+
+        SceneManager.LoadSceneAsync(nextSceneName);
+    }
+}
